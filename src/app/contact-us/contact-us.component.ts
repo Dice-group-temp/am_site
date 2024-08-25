21160d7f1379cf5263d923be9887804498b92f36
Meti A. Bayissa <metiade7@gmail.com>
2024-08-25 23:27:11 +0300

    add contact section

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent {
-
+  images = [
+    {title:'Search', path:'../../assets/img/DBpedia-Logo.png', alt:''},
+    {title:'Search', path:'../../assets/img/GSOC_logo.png',alt:''},
+    {title:'Search', path:'../../assets/img/university_Leipzig_logo.png', alt:''},
+    {title:'Search', path:'../../assets/img/upb-logo-de.svg',alt:''},
+    {title:'Search', path:'../../assets/img/leuphana_logo.png',alt:''},
+  ]
 }
